24c1b91abc522f59b8a9545e71ff66fb7c9795d7
Vincent Radford <vmradford@gmail.com>
2026-02-24 03:52:56 -0500

    chore: removed off phase game loop

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,15 +2,14 @@ import { createBrowserRouter } from 'react-router';
 import { QueryClient } from '@tanstack/react-query';
 import { systemNodeIndexRoutes } from '@/app/router/system-node-index';
 import { adminRoutes } from '@/app/router/admin';
-// import { liteGamePhaseRoutes } from '@/app/router/lite-game/phase';
 import { liteGameCodexRoutes } from '@/app/router/lite-game/codex';
 import { liteGameLoopRoutes } from '@/app/router/lite-game/loop';
 
 export const createAppRouter = (queryClient: QueryClient) =>
+  queryClient;
   createBrowserRouter([
     ...systemNodeIndexRoutes,
     ...adminRoutes,
-    // ...liteGamePhaseRoutes(queryClient),
     ...liteGameCodexRoutes,
     ...liteGameLoopRoutes,
   ]);
